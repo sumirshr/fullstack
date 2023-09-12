d6e18cdbe81ab0283c1833ecb1df1e4894861af0
SumirPasal <sumir.pasalnp@gmail.com>
2023-09-12 16:24:41 +0545

    authentication and token generation, added client

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const mongoose=require("mongoose");
 const bcrypt=require("bcrypt");
 
@@ -25,7 +26,15 @@ const userSchema= new mongoose.Schema({
     cpassword:{
         type:String,
         required:true
-    }
+    },
+    tokens:[
+        {
+           token:{
+            type:String,
+            required:true
+           } 
+        }
+    ]
 })
 
 
@@ -38,6 +47,19 @@ userSchema.pre('save',async function(next){
     }
     next();
 })
+
+// generating token
+    userSchema.methods.generateAuthToken=async function(){
+        try{
+            let token=jwt.sign({_id:this._id},process.env.SECRET_KEY)
+            this.tokens=this.tokens.concat({token:token});
+            await this.save();
+            return token
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
 
 const User=mongoose.model('USERS',userSchema);
 module.exports=User;
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 require("../db/con");
 const User = require("../models/userSchema");
@@ -16,9 +18,11 @@ router.post("/register", async (req, res) => {
     const userExist = await User.findOne({ email: email });
     if (userExist) {
       return res.status(422).json({ status: "email already exists" });
+    } else if (password != cpassword) {
+      return res.status(422).json({ status: "passwords do not match" });
     }
     const user = new User({ name, phone, work, email, password, cpassword });
-    
+
     const userRegister = await user.save();
     if (userRegister) {
       return res.status(201).json({ message: "REGISTERED SUCCESSFULLY" });
@@ -28,25 +32,32 @@ router.post("/register", async (req, res) => {
   }
 });
 
-router.post("/signin", async(req, res) => {
-   
-    try{
-const {email,password}=req.body;
-if (!email || !password) {
-    return res.status(422).json({ status: "field empty" });
+router.post("/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(422).json({ status: "field empty" });
+    }
+    const userLogin = await User.findOne({ email: email });
+    if (userLogin) {
+      const verified = await bcrypt.compare(password, userLogin.password);
+      const token=await userLogin.generateAuthToken();
+      console.log(token)
+      res.cookie("jwtoken",token,{
+        expires:new Date(Date.now()+ 25892000000),
+        httpOnly:true
+      })
+      if (verified) {
+        res.json({ message: "signed in successfully" });
+      } else {
+        res.status(400).json({ message: "invalid credentials" });
+      }
+    } else {
+      res.status(400).json({ error: "invalid credentials" });
+    }
+  } catch (err) {
+    console.log(err);
   }
-  const userLogin = await User.findOne({email:email});
-
-  console.log(userLogin);
-  if(userLogin){
-      res.json({message:'signed in successfully'})
-}else{
-    res.status(400).json({message:'signin error'})
-}
-    }
-    catch(err){
-        console.log(err);
-    }
 });
 
 router.get("/about", (req, res) => {
